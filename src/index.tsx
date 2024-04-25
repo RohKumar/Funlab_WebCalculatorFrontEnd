3314d053df4b8a5bef831f30399d428a0356d94f
rohkumar <rohit@idea2network.com.au>
2024-04-26 09:40:50 +1000

    "Added Advanced Calculator"

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Calculator from './Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Calculator />
+    
   </React.StrictMode>
 );
 
